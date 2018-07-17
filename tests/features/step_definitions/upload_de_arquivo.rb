3c7ba6f834d4fe24465ee79973baee2ac720b79c
Bruno Oliveira <bruoacks@gmail.com>
2018-07-17 01:24:30 -0300

    Conclucao de exercicio

--- tests/features/step_definitions/upload_de_arquivo.rb
+++ tests/features/step_definitions/upload_de_arquivo.rb
@@ -1,6 +1,10 @@
 Quando("eu faco um upload de arquivo") do
 visit '/outros/uploaddearquivos'
+#primeira opcao
+ #attach_file('upload', '/Users/bruno_oliveira/Documents/Ruby/cucumber/capybaratest/tests/features/image.png',make_visible: true)
+#segunda opcao
 
-attach_file('upload', '/Users/bruno_oliveira/Documents/Ruby/cucumber/capybaratest/tests/features/image.png',make_visible: true)
+@foto = File.join(Dir.pwd,'features/image.png' )
+attach_file('upload', @foto ,make_visible: true)
 sleep(4)
 end
